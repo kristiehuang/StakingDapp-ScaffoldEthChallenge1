bbb213fa9a7b2141ea4d3aeb4f5e81b52c759b9b
Kristie Huang <kristiehuang01@gmail.com>
2021-09-28 22:30:09 -0700

    Add stake() fxn

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -6,6 +6,13 @@ import "./ExampleExternalContract.sol"; //https://github.com/OpenZeppelin/openze
 contract Staker {
 
   ExampleExternalContract public exampleExternalContract;
+  mapping (address => uint256) public balances;
+  uint256 public constant threshold = 1 ether;
+
+  event Stake (
+    address staker,
+    uint256 balance
+  );
 
   constructor(address exampleExternalContractAddress) public {
     exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
@@ -13,6 +20,10 @@ contract Staker {
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+  function stake() public payable {
+    balances[msg.sender] += msg.value;
+    emit Stake(msg.sender, msg.value);
+  }
 
 
   // After some `deadline` allow anyone to call an `execute()` function
